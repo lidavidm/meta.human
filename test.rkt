28edd30b316b981621ddb7111a96f3f392c7ac8f
David Li <li.davidm96@gmail.com>
2016-03-03 19:15:03 -0500

    wip: add room system

--- test.rkt
+++ test.rkt
@@ -2,7 +2,21 @@
 
 (require yaml)
 
+(yaml-struct game-state (player rooms current-room))
 (yaml-struct player (name health cpu))
+(yaml-struct room (id description adjacent))
+
+(define (add-room rooms room)
+  (hash-set! rooms (room-id room) room))
+
+(define (get-room rooms room)
+  (hash-ref rooms room))
+
+(define (enter-room state room-id)
+  (begin
+    (show "------------------------")
+    (show (room-description (get-room (game-state-rooms state) room-id)))
+    (struct-copy game-state state [current-room room-id])))
 
 (define (default-prompt [text "meta.human> "])
   (let ([result (begin
@@ -32,33 +46,41 @@
     (show (player-name player))
     (show "HP" (player-health player))))
 
-(define (create-character-loop)
+(define (create-character-loop state)
   (begin
     (show "You're next up in line.")
     (show "Customs Officer: What is your name?")
     (let ([name (prompt "name> ")])
       (begin
         (show "Customs Officer: Very well, proceed on.")
-        (game-loop (player name 100 100))
+        (game-loop
+         (enter-room
+          (struct-copy game-state state [player (player name 100 100)])
+          "intro"))
         )
       )
     )
   )
 
-(define (game-loop character)
-  (let ([command (prompt)])
-    (match command
-      ["help" (begin
-               (show "Help")
-               )]
-      ["status" (display-player character)])
-    (game-loop character)
-    ))
+(define (game-loop state)
+  (begin
+    (let ([command (prompt)])
+      (match command
+        ["help" (begin
+                  (show "Help")
+                  )]
+        ["status" (display-player (game-state-player state))]
+        [_ (show "Invalid command.")])
+      (game-loop state)
+      )))
 
 (define (mainloop)
+  (define rooms (make-hash))
+  (add-room rooms (room "intro" "Description" null))
+  (define initial-state (game-state (player "" 100 100) rooms (get-room rooms "intro")))
   (call-with-continuation-prompt (lambda ()
                                    (parameterize ([prompt-parameter default-prompt]
                                                   [show-parameter default-show])
-                                     (create-character-loop)))))
+                                     (create-character-loop initial-state)))))
 
 (mainloop)
