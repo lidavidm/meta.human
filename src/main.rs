2eeb9ce1dca7bace5aed951791481dbf0eb4024e
David Li <li.davidm96@gmail.com>
2016-04-03 13:34:59 -0400

    refactor: move game logic to its own struct

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
 #![feature(braced_empty_structs)]
 extern crate ncurses;
 
-mod ui;
-mod state;
-mod room;
 mod player;
+mod room;
+mod state;
+mod ui;
 
 use ncurses::*;
-
-use ui::input::InputHandler;
-use ui::window::{ScrollingOutput, WindowLike};
-
-struct DefaultInputHandler {
-
-}
-
-impl InputHandler for DefaultInputHandler {
-}
 
 fn main() {
     let locale_conf = LcCategory::all;
@@ -30,59 +20,23 @@ fn main() {
 
     refresh();
 
-    let input_handler = DefaultInputHandler {};
-
-    let (width, height) = ui::term_size();
-    let main_width = 2 * width / 3;
-    let mut border_win = ui::window::Window::new(0, 3, main_width, height - 6);
-    border_win.box_(0, 0);
-    border_win.refresh();
-    let mut main_win = ui::window::Window::new(1, 4, main_width - 2, height - 8);
-    main_win.refresh();
-
-    let mut output = ui::window::Pad::new(100, 80);
-
-    let mut char_info = ui::window::Window::new(main_width, 0, width - main_width, height);
-    char_info.box_(0, 0);
-    char_info.refresh();
-
-    let mut title_win = ui::window::Window::new(0, 0, main_width, 3);
-    title_win.box_(0, 0);
-    title_win.refresh();
-
-    let mut input_win = ui::window::Window::new(0, height - 3, main_width, 3);
-    input_win.box_(0, 0);
-    input_win.refresh();
+    let mut game = state::Game::new();
 
     let room = room::Room {
         name: "Border Office".to_owned(),
         description: "Florescent light glares off the metal detector ahead.".to_owned(),
     };
-    let world = state::World {};
-    let state = state::State {
-        room: room,
-    };
+    game.world.add_room("border_office_1", room);
+    game.enter_room("border_office_1");
+    game.main();
 
-    output.append("ACT I—HELLO WORLD");
-    output.append("=================");
-    output.append("");
-    output.append(&state.room.description);
-    output.render(&main_win);
+    // output.append("ACT I—HELLO WORLD");
+    // output.append("=================");
+    // output.append("");
+    // output.render(&main_win);
 
-    title_win.print(0, &format!("Location: {:20} 17:07:17 MON 25 MAR 2048", state.room.name));
 
-    loop {
-        let input = input_handler.get_line(&input_win).unwrap();
-
-        match input.as_ref() {
-            "exit" => break,
-            "describe" => output.append(&state.room.description),
-            _ => {
-                output.append(input.as_ref());
-            },
-        }
-        output.render(&main_win);
-    }
+    // title_win.print(0, &format!("Location: {:20} 17:07:17 MON 25 MAR 2048", cur_room.name));
 
     endwin();
 }
--- src/state.rs
+++ src/state.rs
@@ -1,10 +1,129 @@
+use std::cell::RefCell;
+use std::collections::HashMap;
+
 use room;
 
+use ui;
+use ui::input::InputHandler;
+use ui::window::{Pad, ScrollingOutput, Window, WindowLike};
+
 pub struct World {
-    // rooms: ,
+    rooms: HashMap<String, room::Room>,
 }
 
-pub struct State {
+impl World {
+    pub fn new() -> World {
+        World {
+            rooms: HashMap::new(),
+        }
+    }
+
+    pub fn add_room<S: Into<String>>(&mut self, id: S, room: room::Room) {
+        self.rooms.insert(id.into(), room);
+    }
+
+    pub fn get_room(&self, id: &str) -> Option<&room::Room> {
+        self.rooms.get(id)
+    }
+}
+
+struct UILayout {
+    border: Window,
+    main: Window,
+    output: RefCell<Pad>,
+    character: Window,
+    title: Window,
+    input: Window,
+}
+
+impl UILayout {
+    fn new() -> UILayout {
+        let (width, height) = ui::term_size();
+        let main_width = 2 * width / 3;
+        let mut border_win = Window::new(0, 3, main_width, height - 6);
+        border_win.box_(0, 0);
+        border_win.refresh();
+        let mut main_win = Window::new(1, 4, main_width - 2, height - 8);
+        main_win.refresh();
+
+        let mut output = Pad::new(100, 80);
+
+        let mut char_info = Window::new(main_width, 0, width - main_width, height);
+        char_info.box_(0, 0);
+        char_info.refresh();
+
+        let mut title_win = Window::new(0, 0, main_width, 3);
+        title_win.box_(0, 0);
+        title_win.refresh();
+
+        let mut input_win = Window::new(0, height - 3, main_width, 3);
+        input_win.box_(0, 0);
+        input_win.refresh();
+
+        UILayout {
+            border: border_win,
+            main: main_win,
+            output: RefCell::new(output),
+            character: char_info,
+            title: title_win,
+            input: input_win,
+        }
+    }
+
+    fn refresh(&self) {
+        self.output.borrow_mut().render(&self.main);
+    }
+
+    fn display(&self, text: &str) {
+        self.output.borrow_mut().append(text);
+    }
+}
+
+impl InputHandler for UILayout {
+
+}
+
+pub struct Game {
     // player: ,
-    pub room: room::Room,
+    pub world: World,
+    ui: UILayout,
+    pub room: String,
+}
+
+impl Game {
+    pub fn new() -> Game {
+        Game {
+            ui: UILayout::new(),
+            world: World::new(),
+            room: "NO_ROOM_SET".to_owned(),
+        }
+    }
+
+    fn current_room(&self) -> &room::Room {
+        self.world.get_room(&self.room).expect(&format!("Invalid room {}", self.room))
+    }
+
+    pub fn enter_room(&mut self, room: &str) {
+        if !self.world.rooms.contains_key(room) {
+            panic!("Entered room {} that doesn't exist", room);
+        }
+        self.room = room.into();
+        self.ui.display(&self.current_room().description);
+    }
+
+    pub fn main(&mut self) {
+        self.ui.refresh();
+        loop {
+            let input = self.ui.get_line(&self.ui.input).unwrap();
+
+            match input.as_ref() {
+                "exit" => break,
+                "describe" => self.ui.display(&self.current_room().description),
+                _ => {
+                    self.ui.display(input.as_ref());
+                },
+            }
+            self.ui.refresh();
+        }
+    }
 }
